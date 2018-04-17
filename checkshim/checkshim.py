9f7234860644b2f29a3f04bfbed3ae6c762d71ca
Michael H <Sinbad@michaelhall.tech>
2018-04-17 00:13:15 -0400

    Update checkshim.py

--- checkshim/checkshim.py
+++ checkshim/checkshim.py
@@ -5,13 +5,19 @@ class CheckShim:
     def __init__(self, bot):
         self.bot = bot
         self.disallowed = ['contact']
+        self.restricted = ['Audio']
+        self.my_guilds = [355902355868483584, 78634202018357248]
 
     async def __global_check(self, ctx):
-        if not await ctx.bot.is_owner(
+        if await ctx.bot.is_owner(
             ctx.author
-        ) and any(
+        ):
+            return True
+        if any(
             ctx.command == self.bot.get_command(x)
             for x in self.disallowed
         ):
             return False
+        if ctx.cog.__name__ in self.restricted:
+            return ctx.guild.id in self.my_guilds if ctx.guild else False
         return True
